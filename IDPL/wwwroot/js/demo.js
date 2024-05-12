61559176bf6550f000506a994ab4b020a8909b1c
AjayVarmaDev <161057665+AjayVarmaDev@users.noreply.github.com>
2024-05-12 19:29:31 +0530

    basic demo buttons and textbox

---
+++ IDPL/wwwroot/js/demo.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    //alert("1");
+});
+
+function ringAlarm() {
+   // alert("2");
+    // Play sound
+    var txt = document.getElementById('txtAlarm').value;
+    if (txt == 'ajay') {
+        startAlarm();
+    }
+    
+    
+}
+
+
+var audio = new Audio('audio/alarm.mp3'); // Relative path to the audio file
+var alarmButton = document.getElementById('alarmButton');
+// Function to start the alarm
+function startAlarm() {
+    // Start audio playback
+    audio.loop = true; // Loop the audio
+    audio.play();
+    alarmButton.classList.add('blinking');
+    //alert("Alarm activated!");
+}
+
+// Function to stop the alarm
+function stopAlarm() {
+    // Stop audio playback
+    audio.pause();
+    audio.currentTime = 0; // Reset audio to beginning
+    alert("Alarm stopped!");
+    alarmButton.classList.remove('blinking');
+}
