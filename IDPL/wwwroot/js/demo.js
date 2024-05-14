a41fbac968334ba0643f5680c1023913b99b638d
AjayVarmaDev <161057665+AjayVarmaDev@users.noreply.github.com>
2024-05-14 20:25:49 +0530

    code change

--- IDPL/wwwroot/js/demo.js
+++ IDPL/wwwroot/js/demo.js
@@ -13,11 +13,26 @@ function ringAlarm() {
     
 }
 
+var baseUrl = window.location.origin;
 
-var audio = new Audio('audio/alarm.mp3'); // Relative path to the audio file
+// Construct the relative path to the audio file
+var audioUrl = baseUrl + '/audio/alarm.mp3';
+
+// Create an Audio object with the constructed URL
+var audio = new Audio(audioUrl);
+
+// Relative path to the audio file
 var alarmButton = document.getElementById('alarmButton');
 // Function to start the alarm
 function startAlarm() {
+    // Get the base URL of the current page
+   
+
+    //var audio = new Audio('~/audio/alarm.mp3'); 
+
+    audio.addEventListener('play', function () {
+        console.log('Audio playback started');
+    });
     // Start audio playback
     audio.loop = true; // Loop the audio
     audio.play();
@@ -27,9 +42,10 @@ function startAlarm() {
 
 // Function to stop the alarm
 function stopAlarm() {
+    //var audio = new Audio('audio/alarm.mp3'); 
     // Stop audio playback
     audio.pause();
     audio.currentTime = 0; // Reset audio to beginning
-    alert("Alarm stopped!");
+    showBSAlert(__WARNING, "Alarm Stopped", __WARNING);
     alarmButton.classList.remove('blinking');
 }
