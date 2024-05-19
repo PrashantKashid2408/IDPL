1c745874212b02edc4b4a92199c98a8817428ebd
AjayVarmaDev <161057665+AjayVarmaDev@users.noreply.github.com>
2024-05-19 13:21:27 +0530

    code changes

--- IDPL/Controllers/HomeController.cs
+++ IDPL/Controllers/HomeController.cs
@@ -5,10 +5,11 @@ using IDPL.Resources;
 using Microsoft.AspNetCore.Mvc;
 using NModbus;
 
-
+using System.Timers;
 using NModbus.Device;
 using System.Diagnostics;
 using System.Net.Sockets;
+using Timer = System.Timers.Timer;
 
 namespace IDPL.Controllers
 {
@@ -23,7 +24,7 @@ namespace IDPL.Controllers
         //public static ModbusClient modbus = null;
         public static System.Timers.Timer tmr_Read;
         public static System.Timers.Timer tmr_Manipulation;
-
+        private static System.Timers.Timer aTimer;
         public static bool allowToRead = true;
 
         public static ushort[] Values1, Values2, Values3;
@@ -53,191 +54,299 @@ namespace IDPL.Controllers
 
         //public static bool conn = false;
 
-        public IActionResult Index()
+
+
+        private static Timer _timer;
+
+        public async Task<IActionResult> Index()
         {
-            bool issuccess = ReadCheck();
-            if (issuccess)
-            {
-                ViewBag.AddLevel = "true";
-            }
-            else
-            {
-                ViewBag.AddLevel = "false";
-            }
+            // Initial page load
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public async Task<JsonResult> GetModbusValue()
         {
-            return View();
+            int modbusValue = await GetModbusValueAsync();
+            return Json(new { value = modbusValue });
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        private async Task<int> GetModbusValueAsync()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public static bool ReadCheck()
-        {
-            bool issuccess = false;
-            if (allowToRead)
-            {
-                try
-                {
-                    using (TcpClient tcpClient = new TcpClient())
-                    {
-                        tcpClient.Connect("192.168.1.200", 502);
-                        if (tcpClient.Connected)
-                        {
-                            // The socket is connected
-                            Console.WriteLine("Connected to the Modbus server.");
-
-                            // Perform Modbus operations here
-                        }
-                        ModbusFactory modbusFactory = new ModbusFactory();
-                        IModbusMaster modbus = modbusFactory.CreateMaster(tcpClient);
-
-                        //IModbusMaster modbus = null;
-
-
-
-                        byte slaveAddress1 = 1;
-                        ushort startAddress1 = 1;
-                        ushort startAddress2 = 200;
-                        ushort startAddress3 = 300;
-                        ushort numberOfPoints1 = 60;
-                        ushort numberOfPoints2 = 70;
-
-                        ushort[] ushortArray1 = modbus.ReadHoldingRegisters(slaveAddress1, startAddress1, numberOfPoints1);
-                        ushort[] ushortArray2 = modbus.ReadHoldingRegisters(slaveAddress1, startAddress2, numberOfPoints2);
-                        ushort[] ushortArray3 = modbus.ReadHoldingRegisters(slaveAddress1, startAddress3, numberOfPoints2);
-
-                        byte[] byteArray1 = new byte[ushortArray1.Length * 2];
-                        Buffer.BlockCopy(ushortArray1, 0, byteArray1, 0, byteArray1.Length);
-                        byte[] byteArray2 = new byte[ushortArray2.Length * 2];
-                        Buffer.BlockCopy(ushortArray2, 0, byteArray2, 0, byteArray2.Length);
-                        byte[] byteArray3 = new byte[ushortArray3.Length * 2];
-                        Buffer.BlockCopy(ushortArray3, 0, byteArray3, 0, byteArray3.Length);
-
-
-                        if ((double)ushortArray1[11] > 4999)
-                        {
-                            issuccess = true;
-                        }
-                        else
-                        {
-                            issuccess = false;
-                        }
-                        //CO2 Value In Slave 4
-                        //for (i = 11; i < 12; i++)
-                        //{
-                        //    if (ScadaWindow.co2 == 250)
-                        //    {
-                        //        analogValue[i] = (double)(Values1[i] * 0.0625);
-                        //    }
-
-                        //    if (ScadaWindow.co2 == 500)
-                        //    {
-                        //        analogValue[i] = (double)(Values1[i] * 0.125);
-                        //    }
-                        //}
-
-                        //Values1 = byteArray1;
-                        //Values2 = byteArray2;
-                        //Values3 = byteArray3;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    conn = false;
-                }
-                
-            }
-            return issuccess;
-
-
-        }
-        // public static void write_single_reg(byte slave_no, string coil_No, short state)
-        public IActionResult TurnOnOffLed(  int id = 0)
-        {
-            //IModbusMaster modbus = null;
-
-
-
-            //using (TcpClient tcpClient = new TcpClient())
-            //{
-            //    tcpClient.Connect("192.168.1.200", 502);
-            //    if (tcpClient.Connected)
-            //    {
-            //        // The socket is connected
-            //        Console.WriteLine("Connected to the Modbus server.");
-
-            //        // Perform Modbus operations here
-            //    }
-            //    ModbusFactory modbusFactory = new ModbusFactory();
-            //    IModbusMaster modbus = modbusFactory.CreateMaster(tcpClient);
-            //    //modbus = new ModbusTCPMaster(plcIpAddress, 502);
-            //    //modbus.Connection();
-
-            //    if (modbus != null)
-            //    {
-            //        byte slaveAdd = slave_no;
-            //        string startCoilAdd = coil_No;
-            //        short on_off_state = state;
-
-            //        modbus.WriteSingleRegister(slaveAdd, startCoilAdd, on_off_state);
-            //    }
-            //}
-            JsonMessage _jsonMessage = null;
-            string plcIpAddress = "192.168.1.200";
-            ushort port = 502;
-            byte slaveNo = 1; // Replace with your PLC's slave address
-            ushort coilAddress = 1; // Replace with your coil address
-            bool coilValue = true; // Replace with the state you want to write (true or false)
-
-            if(id == 1)
-            {
-                coilValue = true;
-            }
-            else if(id == 0)
-            {
-                coilValue = false;
-            }
+            int modbusValue = 0;
 
             try
             {
-                using (TcpClient tcpClient = new TcpClient(plcIpAddress, port))
+                using (TcpClient tcpClient = new TcpClient("122.169.112.64", 502))
                 {
                     ModbusFactory modbusFactory = new ModbusFactory();
                     IModbusMaster modbus = modbusFactory.CreateMaster(tcpClient);
 
-                    if (modbus != null && tcpClient.Connected)
-                    {
-                        modbus.WriteSingleCoil(slaveNo, coilAddress, coilValue);
-                        Console.WriteLine("Write operation successful.");
-                        _jsonMessage = new JsonMessage(true, @Resource.lbl_success, "Led Operation Successful", KeyEnums.JsonMessageType.SUCCESS);
-                        //return true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Failed to create Modbus master or establish connection.");
-                        _jsonMessage = new JsonMessage(false, @Resource.lbl_error, "Led Operation Failed", KeyEnums.JsonMessageType.ERROR);
-                        // return false;
-                    }
+                    byte slaveAddress1 = 1;
+                    ushort startAddress1 = 1;
+                    ushort startAddress2 = 200;
+                    ushort startAddress3 = 300;
+                    ushort numberOfPoints1 = 60;
+                    ushort numberOfPoints2 = 70;
+
+                    ushort[] ushortArray1 = modbus.ReadHoldingRegisters(slaveAddress1, startAddress1, numberOfPoints1);
+                    ushort[] ushortArray2 = modbus.ReadHoldingRegisters(slaveAddress1, startAddress2, numberOfPoints2);
+                    ushort[] ushortArray3 = modbus.ReadHoldingRegisters(slaveAddress1, startAddress3, numberOfPoints2);
+
+                    byte[] byteArray1 = new byte[ushortArray1.Length * 2];
+                    Buffer.BlockCopy(ushortArray1, 0, byteArray1, 0, byteArray1.Length);
+                    byte[] byteArray2 = new byte[ushortArray2.Length * 2];
+                    Buffer.BlockCopy(ushortArray2, 0, byteArray2, 0, byteArray2.Length);
+                    byte[] byteArray3 = new byte[ushortArray3.Length * 2];
+                    Buffer.BlockCopy(ushortArray3, 0, byteArray3, 0, byteArray3.Length);
+
+                    modbusValue = ushortArray1[11];
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("An error occurred: " + ex.Message);
+                // Handle exceptions
+                Console.WriteLine("Error: " + ex.Message);
             }
-            return Json(_jsonMessage);
 
+            return modbusValue;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> ToggleBit([FromBody] BitToggleRequest request)
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient("122.169.112.64", 502))
+                {
+                    var factory = new ModbusFactory();
+                    var master = factory.CreateMaster(client);
+
+                    // Read current value
+                    ushort[] registers = await master.ReadHoldingRegistersAsync(1, (ushort)(request.Address - 40001), 1);
+                    ushort currentValue = registers[0];
+
+                    // Toggle the specified bit
+                    ushort mask = (ushort)(1 << request.BitIndex);
+                    ushort newValue = (ushort)(currentValue ^ mask);
+
+                    // Write the new value
+                    await master.WriteSingleRegisterAsync(1, (ushort)(request.Address - 40001), newValue);
+                }
+
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, error = ex.Message });
+            }
+        }
+
+
     }
-    public class ScadaWindow
+    public class BitToggleRequest
     {
-        public int co2 { get; set; }
+        public int Address { get; set; }
+        public int BitIndex { get; set; }
     }
+
+    //private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    //{
+
+    //    ReadCheck();
+    //}
+
+    //private static void YourMethod()
+    //{
+    //    // Your method logic here
+    //    Console.WriteLine("YourMethod was called.");
+    //}
+
+
+
+
+    //public IActionResult Index()
+    //{
+    //    _timer = new Timer(10000); // 10000 milliseconds = 10 seconds
+
+    //    // Hook up the Elapsed event for the timer
+    //    _timer.Elapsed += OnTimedEvent;
+
+    //    // Start the timer in the background
+    //    Task.Run(() => _timer.Start());
+    //    // bool issuccess = ReadCheck();
+    //    bool issuccess = true;
+    //    if (issuccess)
+    //    {
+    //        ViewBag.AddLevel = "true";
+    //    }
+    //    else
+    //    {
+    //        ViewBag.AddLevel = "false";
+    //    }
+    //    return View();
+    //}
+
+    //public IActionResult Privacy()
+    //{
+    //    return View();
+    //}
+
+    //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    //public IActionResult Error()
+    //{
+    //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    //}
+
+    //public static bool ReadCheck()
+    //{
+    //    bool issuccess = false;
+    //    if (allowToRead)
+    //    {
+    //        try
+    //        {
+    //            using (TcpClient tcpClient = new TcpClient())
+    //            {
+    //                // tcpClient.Connect("192.168.1.200", 502);122.169.112.64
+    //                tcpClient.Connect("122.169.112.64", 502); 
+    //                if (tcpClient.Connected)
+    //                {
+    //                    // The socket is connected
+    //                    Console.WriteLine("Connected to the Modbus server.");
+
+    //                    // Perform Modbus operations here
+    //                }
+    //                ModbusFactory modbusFactory = new ModbusFactory();
+    //                IModbusMaster modbus = modbusFactory.CreateMaster(tcpClient);
+
+    //                //IModbusMaster modbus = null;
+
+
+
+    //                byte slaveAddress1 = 1;
+    //                ushort startAddress1 = 1;
+    //                ushort startAddress2 = 200;
+    //                ushort startAddress3 = 300;
+    //                ushort numberOfPoints1 = 60;
+    //                ushort numberOfPoints2 = 70;
+
+    //                ushort[] ushortArray1 = modbus.ReadHoldingRegisters(slaveAddress1, startAddress1, numberOfPoints1);
+    //                ushort[] ushortArray2 = modbus.ReadHoldingRegisters(slaveAddress1, startAddress2, numberOfPoints2);
+    //                ushort[] ushortArray3 = modbus.ReadHoldingRegisters(slaveAddress1, startAddress3, numberOfPoints2);
+
+    //                byte[] byteArray1 = new byte[ushortArray1.Length * 2];
+    //                Buffer.BlockCopy(ushortArray1, 0, byteArray1, 0, byteArray1.Length);
+    //                byte[] byteArray2 = new byte[ushortArray2.Length * 2];
+    //                Buffer.BlockCopy(ushortArray2, 0, byteArray2, 0, byteArray2.Length);
+    //                byte[] byteArray3 = new byte[ushortArray3.Length * 2];
+    //                Buffer.BlockCopy(ushortArray3, 0, byteArray3, 0, byteArray3.Length);
+
+    //                int index = 11 * 2;
+
+    //                // Access the bytes
+    //                byte lowerByte = byteArray1[index]; // byteArray1[22]
+    //                byte higherByte = byteArray1[index + 1]; // byteArray1[23]
+
+    //                // Combine the bytes to get back the ushort value
+    //                ushort reconstructedValue = (ushort)(lowerByte | (higherByte << 8));
+
+
+
+    //                if ((double)ushortArray1[11] > 4999)
+    //                {
+    //                    issuccess = true;
+    //                }
+    //                else
+    //                {
+    //                    issuccess = false;
+    //                }
+
+    //            }
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            conn = false;
+    //        }
+
+    //    }
+    //    return issuccess;
+
+
+    //}
+    //// public static void write_single_reg(byte slave_no, string coil_No, short state)
+    //public IActionResult TurnOnOffLed(  int id = 0)
+    //{
+    //    //IModbusMaster modbus = null;
+
+
+
+    //    //using (TcpClient tcpClient = new TcpClient())
+    //    //{
+    //    //    tcpClient.Connect("192.168.1.200", 502);
+    //    //    if (tcpClient.Connected)
+    //    //    {
+    //    //        // The socket is connected
+    //    //        Console.WriteLine("Connected to the Modbus server.");
+
+    //    //        // Perform Modbus operations here
+    //    //    }
+    //    //    ModbusFactory modbusFactory = new ModbusFactory();
+    //    //    IModbusMaster modbus = modbusFactory.CreateMaster(tcpClient);
+    //    //    //modbus = new ModbusTCPMaster(plcIpAddress, 502);
+    //    //    //modbus.Connection();
+
+    //    //    if (modbus != null)
+    //    //    {
+    //    //        byte slaveAdd = slave_no;
+    //    //        string startCoilAdd = coil_No;
+    //    //        short on_off_state = state;
+
+    //    //        modbus.WriteSingleRegister(slaveAdd, startCoilAdd, on_off_state);
+    //    //    }
+    //    //}
+    //    JsonMessage _jsonMessage = null;
+    //    string plcIpAddress = "192.168.1.200";
+    //    ushort port = 502;
+    //    byte slaveNo = 1; // Replace with your PLC's slave address
+    //    ushort coilAddress = 1; // Replace with your coil address
+    //    bool coilValue = true; // Replace with the state you want to write (true or false)
+
+    //    if(id == 1)
+    //    {
+    //        coilValue = true;
+    //    }
+    //    else if(id == 0)
+    //    {
+    //        coilValue = false;
+    //    }
+
+    //    try
+    //    {
+    //        using (TcpClient tcpClient = new TcpClient(plcIpAddress, port))
+    //        {
+    //            ModbusFactory modbusFactory = new ModbusFactory();
+    //            IModbusMaster modbus = modbusFactory.CreateMaster(tcpClient);
+
+    //            if (modbus != null && tcpClient.Connected)
+    //            {
+    //                modbus.WriteSingleCoil(slaveNo, coilAddress, coilValue);
+    //                Console.WriteLine("Write operation successful.");
+    //                _jsonMessage = new JsonMessage(true, @Resource.lbl_success, "Led Operation Successful", KeyEnums.JsonMessageType.SUCCESS);
+    //                //return true;
+    //            }
+    //            else
+    //            {
+    //                Console.WriteLine("Failed to create Modbus master or establish connection.");
+    //                _jsonMessage = new JsonMessage(false, @Resource.lbl_error, "Led Operation Failed", KeyEnums.JsonMessageType.ERROR);
+    //                // return false;
+    //            }
+    //        }
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        Console.WriteLine("An error occurred: " + ex.Message);
+    //    }
+    //    return Json(_jsonMessage);
+
+    //}
 }
--- IDPL/wwwroot/js/demo.js
+++ IDPL/wwwroot/js/demo.js
@@ -1,54 +1,54 @@
 ﻿
 
-function ringAlarm(id,id2) {
-    // alert("2");
-    // Play sound
-    //id2 = "true";
-    var txt = document.getElementById('txtAlarm').value;
-    if (txt == 'a' || id2 == true) {
-        startAlarm();
-    }
+//function ringAlarm(id,id2) {
+//    // alert("2");
+//    // Play sound
+//    //id2 = "true";
+//    var txt = document.getElementById('txtAlarm').value;
+//    if (id == true) {
+//        startAlarm();
+//    }
 
 
-}
+//}
 
-var baseUrl = window.location.origin;
+//var baseUrl = window.location.origin;
 
-// Construct the relative path to the audio file
-var audioUrl = baseUrl + '/audio/alarm.mp3';
+//// Construct the relative path to the audio file
+//var audioUrl = baseUrl + '/audio/alarm.mp3';
 
-// Create an Audio object with the constructed URL
-var audio = new Audio(audioUrl);
+//// Create an Audio object with the constructed URL
+//var audio = new Audio(audioUrl);
 
-// Relative path to the audio file
+//// Relative path to the audio file
 
-// Function to start the alarm
-function startAlarm() {
-    // Get the base URL of the current page
+//// Function to start the alarm
+//function startAlarm() {
+//    // Get the base URL of the current page
 
-    var alarmButton = document.getElementById('alarmButton');
-    //var audio = new Audio('~/audio/alarm.mp3'); 
+//    var alarmButton = document.getElementById('alarmButton');
+//    //var audio = new Audio('~/audio/alarm.mp3');
 
-    audio.addEventListener('play', function () {
-        console.log('Audio playback started');
-    });
-    // Start audio playback
-    audio.loop = true; // Loop the audio
-    audio.play();
-    alarmButton.classList.add('blinking');
-    //alert("Alarm activated!");
-}
+//    audio.addEventListener('play', function () {
+//        console.log('Audio playback started');
+//    });
+//    // Start audio playback
+//    audio.loop = true; // Loop the audio
+//    audio.play();
+//    alarmButton.classList.add('blinking');
+//    //alert("Alarm activated!");
+//}
 
-// Function to stop the alarm
-function stopAlarm() {
-    //var audio = new Audio('audio/alarm.mp3');
-    // Stop audio playback
-    var alarmButton = document.getElementById('alarmButton');
-    audio.pause();
-    audio.currentTime = 0; // Reset audio to beginning
-    showBSAlert(__WARNING, "Alarm Stopped", __WARNING);
-    alarmButton.classList.remove('blinking');
-}
+//// Function to stop the alarm
+//function stopAlarm() {
+//    //var audio = new Audio('audio/alarm.mp3');
+//    // Stop audio playback
+//    var alarmButton = document.getElementById('alarmButton');
+//    audio.pause();
+//    audio.currentTime = 0; // Reset audio to beginning
+//    showBSAlert(__WARNING, "Alarm Stopped", __WARNING);
+//    alarmButton.classList.remove('blinking');
+//}
 
 
 
@@ -69,39 +69,125 @@ updateTime();
 // Update time every second after the initial call
 setInterval(updateTime, 1000);
 
-function TurnOnOffLed(id) {
+//function TurnOnOffLed(id) {
 
-    if (id == true) {
-        startAlarm();
+//    if (id == true) {
+//        startAlarm();
+//    }
+
+//    var buttonName = $("#ledTurnOnOff")
+//    var id = 0;
+//    var button = document.getElementById("ledTurnOnOff");
+//    if (button.textContent === "Turn Off Led") {
+//        button.textContent = "Turn On Led";
+//        id = 1;
+//    } else {
+//        button.textContent = "Turn Off Led";
+//        id = 0;
+//    }
+
+//    $.ajax({
+//        url: '/Home/TurnOnOffLed',
+//        data: {"id":id},
+//        type: "POST",
+//        cache: false,
+//        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+//        success: function (result) {
+
+//            //  $("#btnUnAssign").attr("class", "btn btn-primary disabled");
+//            // $("#nxtUnassigned").attr("class", "btn btn-primary disabled");
+//        },
+//        error: function (xhr, ajaxOptions, thrownError) {
+//            removeLoader("#divPri1");
+//        }
+//    });
+//}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    var audio = new Audio('/audio/alarm.mp3');
+    var alarmButton = document.getElementById('alarmButton');
+    var manuallyStopped = false;
+    var stopDuration = 60000; // 5 minutes in milliseconds
+    var isautomaticStopped = false;
+
+    function startAlarm() {
+        if (audio.paused) {
+            audio.loop = true;
+            audio.play().catch(function (error) {
+                console.log("Audio playback failed: " + error);
+            });
+        }
+        alarmButton.classList.add('blinking');
     }
 
-    var buttonName = $("#ledTurnOnOff")
-    var id = 0;
-    var button = document.getElementById("ledTurnOnOff");
-    if (button.textContent === "Turn Off Led") {
-        button.textContent = "Turn On Led";
-        id = 1;
-    } else {
-        button.textContent = "Turn Off Led";
-        id = 0;
+    function stopAlarm() {
+        if (!audio.paused) {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+        alarmButton.classList.remove('blinking');
+        manuallyStopped = true;
+        setTimeout(() => {
+            manuallyStopped = false;
+        }, stopDuration);
+        if (isautomaticStopped == false) {
+            showBSAlert(__WARNING, "Alarm Stopped manually", __WARNING);
+        }
     }
-   
-    $.ajax({
-        url: '/Home/TurnOnOffLed',
-        data: {"id":id},
-        type: "POST",
-        cache: false,
-        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-        success: function (result) {
-           
-            //  $("#btnUnAssign").attr("class", "btn btn-primary disabled");
-            // $("#nxtUnassigned").attr("class", "btn btn-primary disabled");
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            removeLoader("#divPri1");
-        }   
+
+    function fetchModbusValue() {
+        fetch('/Home/GetModbusValue')
+            .then(response => response.json())
+            .then(data => {
+                var modbusValue = data.value;
+
+                document.getElementById('modbusValueDisplay').textContent = modbusValue;
+                document.getElementById('alarmThresholdDisplay').textContent = 5000;
+
+
+                if (!manuallyStopped && modbusValue >= 5000) {
+                    startAlarm();
+                } else if (modbusValue < 5000) {
+                    stopAlarm();
+                    isautomaticStopped = true;
+                }
+            })
+            .catch(error => console.error('Error fetching Modbus value:', error));
+    }
+
+    setInterval(fetchModbusValue, 1000);
+
+    alarmButton.addEventListener('click', function () {
+        stopAlarm();
     });
-}
+
+
+    toggleBitButton.addEventListener('click', function () {
+        fetch('/Home/ToggleBit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ address: 40001, bitIndex: 0 })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    alert('Bit toggled successfully');
+                } else {
+                    alert('Failed to toggle bit');
+                }
+            })
+            .catch(error => console.error('Error toggling bit:', error));
+    });
+
+});
+
+
 
 
         
